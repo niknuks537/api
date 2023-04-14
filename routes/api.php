c35e2343e9bcefff8c06ebe3a23d03528e6e698b
Nicko Jay Menecio <nickojay22@gmail.com>
2023-04-14 16:43:25 +1000

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// without this, the code throws an error saying that it cannot find ProductController
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Commented out the line that was suggested in the tutorial
+// Route::apiResource('/products', 'ProductController');
+Route::apiResource('products', ProductController::class);
+
+Route::group(['prefix'=>'products'],function(){
+    Route::apiResource('/{product}/reviews',ReviewController::class);
 });
