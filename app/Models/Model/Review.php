00fce8dcb3dca5c9ada6164544021b3e30462670
Nicko Jay Menecio <nickojay22@gmail.com>
2023-04-18 19:31:07 +1000

    Added the database relationship between Product and Review

--- app/Models/Model/Review.php
+++ app/Models/Model/Review.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    // Added codes
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
